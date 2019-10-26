eb7cea9af2a68d123d490c02ab67856071234d5d
Holberton <holberton@Holbertons-iMac-8.local>
2019-10-26 17:04:10 -0500

    Add holberton Hader

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef header_file
+#define header_file
+#include <stdarg.h>
+typedef struct typeprint
+{
+	char typec;
+	void (*fun)(va_list lista);
+} typedate;
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
+int _printf(const char *format, ...);
+void printString(va_list lista);
+void printChar(va_list lista);
+void printPorce();
+#endif
