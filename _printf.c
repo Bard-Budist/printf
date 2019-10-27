55026624ee984aca0424c6ec4da72fb2b88a368e
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-27 00:50:40 +0000

    add function decimal and integer

--- _printf.c
+++ _printf.c
@@ -48,9 +48,66 @@ void printPorce(void)
 int printDecimal(va_list lista)
 {
 	int cont = 0;
-	long int decimal, cont2 = 1, b;
+	long int decimal, cont2 = 1, num;
 
 	decimal = va_arg(lista, int);
+	if (decimal < 0)
+	{
+		decimal *= -1; /* (decimial * -1) */
+		cont += _putchar('-');
+	}
+	if (decimal == 0)
+	{
+		_putchar('0'); /* ASCII '0 = '48 */
+		return (1);
+	}
+	num = decimal;
+	while (num > 9)
+	{
+		num /= 10;
+		cont2 *= 10;
+	}
+	while (cont2 > 0)
+	{
+		cont += _putchar(((decimal / num) % 10) + '0'); /* ASCII '0 = '48 */
+		num /= 10;
+	}
+	return (cont);
+}
+
+/**
+ * printInteger - Print Decimal
+ * @lista: Var list
+ * Return: number decimal
+ */
+int printInteger(va_list lista)
+{
+	int cont = 0;
+	long int integer, cont2 = 1, num;
+
+	integer = va_arg(lista, int);
+	if (integer < 0)
+	{
+		integer *= -1; /* (decimial * -1) */
+		cont += _putchar('-');
+	}
+	if (integer == 0)
+	{
+		_putchar('0'); /* ASCII '0 = '48 */
+		return (1);
+	}
+	num = integer;
+	while (num > 9)
+	{
+		num /= 10;
+		cont2 *= 10;
+	}
+	while (cont2 > 0)
+	{
+		cont += _putchar(((integer / num) % 10) + '0'); /* ASCII '0 = '48 */
+		num /= 10;
+	}
+	return (cont);
 }
 
 /**
@@ -66,7 +123,8 @@ int _printf(const char *format, ...)
 		{'c', printChar},
 		{'%', printPorce},
 		{'d', printDecimal},
-		{'i', printInteger}
+		{'i', printInteger},
+		{NULL, NULL}
 	};
 	int i = 0, j = 0;
 	va_list list;
@@ -74,7 +132,7 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 	while (format[i])
 	{
-		for (j = 0; j < 3; j++)
+		for (j = 0; j < 6; j++)
 		{
 			if (format[i] == '%' && format[i + 1] == tipos[j].typec)
 			{
