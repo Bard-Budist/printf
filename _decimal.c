c7a11ff012bd916f9860d311dfd3ebb7ef5dfd9a
GiovanniAyala <op7puro@live.com>
2019-10-27 02:58:17 +0000

    Fix files _decimal, _printf and holberton header

--- _decimal.c
+++ _decimal.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * printDecimal - Print Decimal
  * @lista: Var list
--- holberton.h
+++ holberton.h
@@ -10,8 +10,8 @@
  */
 typedef struct typeprint
 {
-    char typec;
-    void (*fun)(va_list lista);
+	char typec;
+	void (*fun)(va_list lista);
 } typedate;
 
 int _strcmp(char *s1, char *s2);
@@ -19,7 +19,7 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 void printString(va_list lista);
 void printChar(va_list lista);
-void printPorce(void);
+void printPorce();
 int printDecimal(va_list lista);
 int printInteger(va_list lista);
 #endif /* Libraries Printf Giovanni and Lina */
