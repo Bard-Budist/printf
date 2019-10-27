5aef53eb748831687080bfedfa996b084948f7c7
GiovanniAyala <op7puro@live.com>
2019-10-27 01:43:11 +0000

    Fix tabs in file

--- _decimal.c
+++ _decimal.c
@@ -6,32 +6,32 @@
  */
 int printDecimal(va_list lista)
 {
-    int cont = 0;
-    long int decimal, cont2 = 1, num;
+	int cont = 0;
+	long int decimal, cont2 = 1, num;
 
-    decimal = va_arg(lista, int);
-    if (decimal < 0)
-    {
-        decimal *= -1; /* (decimial * -1) */
-        cont += _putchar('-');
-    }
-    if (decimal == 0)
-    {
-        _putchar('0'); /* ASCII '0 = '48 */
-        return (1);
-    }
-    num = decimal;
-    while (num > 9)
-    {
-        num /= 10;
-        cont2 *= 10;
-    }
-    while (cont2 > 0)
-    {
-        cont += _putchar(((decimal / num) % 10) + '0'); /* ASCII '0 = '48 */
-        num /= 10;
-    }
-    return (cont);
+	decimal = va_arg(lista, int);
+	if (decimal < 0)
+	{
+		decimal *= -1; /* (decimial * -1) */
+		cont += _putchar('-');
+	}
+	if (decimal == 0)
+	{
+		_putchar('0'); /* ASCII '0 = '48 */
+		return (1);
+	}
+	num = decimal;
+	while (num > 9)
+	{
+		num /= 10;
+		cont2 *= 10;
+	}
+	while (cont2 > 0)
+	{
+		cont += _putchar(((decimal / num) % 10) + '0'); /* ASCII '0 = '48 */
+		num /= 10;
+	}
+	return (cont);
 }
 
 /**
@@ -41,32 +41,31 @@ int printDecimal(va_list lista)
  */
 int printInteger(va_list lista)
 {
-    int cont = 0;
-    long int integer, cont2 = 1, num;
+	int cont = 0;
+	long int integer, cont2 = 1, num;
 
-    integer = va_arg(lista, int);
-    if (integer < 0)
-    {
-        integer *= -1; /* (decimial * -1) */
-        cont += _putchar('-');
-    }
-    if (integer == 0)
-    {
-        _putchar('0'); /* ASCII '0 = '48 */
-        return (1);
-    }
-    num = integer;
-    while (num > 9)
-    {
-        num /= 10;
-        cont2 *= 10;
-    }
-    while (cont2 > 0)
-    {
-        cont += _putchar(((integer / num) % 10) + '0'); /* ASCII '0 = '48 */
-        num /= 10;
-    }
-    return (cont);
+	integer = va_arg(lista, int);
+	if (integer < 0)
+	{
+		integer *= -1; /* (decimial * -1) */
+		cont += _putchar('-');
+	}
+	if (integer == 0)
+	{
+		_putchar('0'); /* ASCII '0 = '48 */
+		return (1);
+	}
+	num = integer;
+	while (num > 9)
+	{
+		num /= 10;
+		cont2 *= 10;
+	}
+	while (cont2 > 0)
+	{
+		cont += _putchar(((integer / num) % 10) + '0'); /* ASCII '0 = '48 */
+		num /= 10;
+	}
+	return (cont);
 }
-
 
