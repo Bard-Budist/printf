5e04e191190b3090517707b87f65326844fb0248
GiovanniAyala <op7puro@live.com>
2019-10-28 18:18:23 -0500

    ReFix of the null

--- _printf.c
+++ _printf.c
@@ -59,7 +59,7 @@ int cont = -1;
 if (format != NULL)
 {
 	va_start(list, format);
-	if (format[0] == '%' && format[1] == '\0' && format == NULL)
+	if (format[0] == '%' && format[1] == '\0')
 	{
 		return (-1);
 	}
