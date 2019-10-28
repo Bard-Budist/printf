40d49f4427a563e4d4efd1fca78fb2060a2a2dd4
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-28 12:47:46 +0000

    space but there's still 40 lines to fix.

--- _printf.c
+++ _printf.c
@@ -6,52 +6,52 @@
  */
 int _printf(const char *format, ...)
 {
-	const typedate tipos[] = {
-		{'s', printString}, {'c', printChar}, {'i', printInteger},
-		{'d', printDecimal}, {'b', printBinary}, {'u', printUnsigned},
-		{'o', printOctal}
-	};
-	int cont = -1;
+const typedate tipos[] = {
+	{'s', printString}, {'c', printChar}, {'i', printInteger},
+	{'d', printDecimal}, {'b', printBinary}, {'u', printUnsigned},
+	{'o', printOctal}
+};
+int cont = -1;
 
-	if (format != NULL)
-	{
-	int i = 0, j = 0;
-	va_list list;
+if (format != NULL)
+{
+int i = 0, j = 0;
+va_list list;
 
-	va_start(list, format);
-	if (format[0] == '%' && format[1] == '\0')
+va_start(list, format);
+if (format[0] == '%' && format[1] == '\0')
+{
+	return (-1);
+}
+cont = 0;
+while (format && format[i])
+{
+	if (format[i] == '%' && format[i + 1] != '%')
 	{
-		return (-1);
-	}
-	cont = 0;
-	while (format && format[i])
-	{
-		if (format[i] == '%' && format[i + 1] != '%')
+		for (j = 0; j < 7; j++)
 		{
-			for (j = 0; j < 7; j++)
+			if (format[i] == '%' && format[i + 1] == tipos[j].typec)
 			{
-				if (format[i] == '%' && format[i + 1] == tipos[j].typec)
-				{
-					cont += tipos[j].fun(list);
-					i++;
-					break;
-				}
+				cont += tipos[j].fun(list);
+				i++;
+				break;
 			}
 		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-			cont += 1;
-		}
-		else
-		{
-			_putchar(format[i]);
-			cont++;
-		}
-		i++;
 	}
-	va_end(list);
-    }
-	return (cont);
+	else if (format[i] == '%' && format[i + 1] == '%')
+	{
+		_putchar('%');
+		i++;
+		cont += 1;
+	}
+	else
+	{
+		_putchar(format[i]);
+		cont++;
+	}
+	i++;
+}
+va_end(list);
+}
+return (cont);
 }
