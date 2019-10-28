e6fa28c0ff6ce5a6b27411a4c17c9fb579e3a2c0
GiovanniAyala <op7puro@live.com>
2019-10-28 10:53:26 -0500

    Comment Hexa and add Description

--- _hexadecimal.c
+++ _hexadecimal.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
- *
- *
+ * printHexadecimalx - printHexa
+ * @lista: Lista
+ * Return: int number
  */
-
 int printHexadecimalx(unsigned int lista)
 {
-/**
-int i, j , cont = 0, aux;
-unsigned int num = lista;
-char *p;
-if (lista == 0)
-        return (_putchar('0'));
-while ()
-{
-}
+/*
+ *int i, j , cont = 0, aux;
+ *unsigned int num = lista;
+ *char *p;
+ *if (lista == 0)
+ *       return (_putchar('0'));
+ *while ()
+ *{
+ *}
 */
 printf("%u", lista);
 return (0);
