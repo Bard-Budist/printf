8da8179da75c6a560c547e68e2d0be3eff43184c
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-28 03:46:56 +0000

    Fix task char and string merge

--- _charandstring.c
+++ _charandstring.c
@@ -6,7 +6,6 @@
  */
 int printString(va_list lista)
 {
-<<<<<<< HEAD
 int i, cont = 0;
 char *string;
 
@@ -18,20 +17,6 @@ for (i = 0; string[i]; i++)
 cont += _putchar(string[i]);
 
 return (cont);
-=======
-	int cs = 0;
-	char *string = va_arg(lista, char*);
-
-	if (string == 0)
-	{
-		while (string[cs] != '\0')
-		{
-			_putchar(string[cs]);
-			cs++;
-		}
-	}
-	return (cs);
->>>>>>> 4291ad51e50f0d171dfe32ae1524d00b2cf47715
 }
 /**
  * printChar - Print String
@@ -40,19 +25,8 @@ return (cont);
  */
 int printChar(va_list lista)
 {
-<<<<<<< HEAD
 int cs = va_arg(lista, int);
 
 return (_putchar(cs));
-=======
-	char chart = va_arg(lista, int);
-
-	if (chart == 0)
-	{
-		_putchar(chart);
-		return (1);
-	}
-	return (0);
->>>>>>> 4291ad51e50f0d171dfe32ae1524d00b2cf47715
 }
 
