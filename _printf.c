b21298594a8d7c337ae65b87f3834500254cd714
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-28 12:45:35 +0000

    Fix task prinf between unsigned and octal for identation problem betty more line 40

--- _printf.c
+++ _printf.c
@@ -11,21 +11,24 @@ int _printf(const char *format, ...)
 		{'d', printDecimal}, {'b', printBinary}, {'u', printUnsigned},
 		{'o', printOctal}
 	};
-        int cont = -1;
-        if (format != NULL)
-        {
+	int cont = -1;
+
+	if (format != NULL)
+	{
 	int i = 0, j = 0;
 	va_list list;
-        
+
 	va_start(list, format);
-        if (format[0] == '%' && format[1] == '\0')
-                return (-1);
-        cont = 0;
+	if (format[0] == '%' && format[1] == '\0')
+	{
+		return (-1);
+	}
+	cont = 0;
 	while (format && format[i])
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
 		{
-			for (j = 0; j < 5; j++)
+			for (j = 0; j < 7; j++)
 			{
 				if (format[i] == '%' && format[i + 1] == tipos[j].typec)
 				{
@@ -49,6 +52,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(list);
-        }
+    }
 	return (cont);
 }
