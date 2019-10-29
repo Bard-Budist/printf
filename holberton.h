c9b5d9a8681a5b16875965f2afd4d681fbc25463
GiovanniAyala <op7puro@live.com>
2019-10-29 15:36:26 -0500

    Fix header and add description in Buffer

--- holberton.h
+++ holberton.h
@@ -32,5 +32,5 @@ int printUnsigned(va_list lista);
 int printUnsignedDigit(unsigned int num, int *cont);
 int printOctal(va_list lista);
 int printDigitOctal(unsigned int num, int *cont);
-char *_littleBuffer(void);
+char *_littleBuffer(int size);
 #endif /* Libraries Printf Giovanni and Lina */
